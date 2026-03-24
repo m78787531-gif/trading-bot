0780f25a4d529931de43e763cdbbc1202a7462b0
m78787531-gif <m78787531@gmail.com>
2026-03-24 14:03:35 +0530

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TradingDashboard from '../TradingDashboard.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TradingDashboard />
+  </React.StrictMode>,
+)
